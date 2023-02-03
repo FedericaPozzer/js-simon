6027ab25a1a33b72661784b57a0f17555d4401fb
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-03 14:24:45 +0100

    init project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// console.log("Hello Kik");
+
+
+// Traccia
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+// Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+
+
+// deve partire subito, quindi setInterval
