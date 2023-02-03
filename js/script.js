1696cbc028c80712aee7e9175257fcfd6367a26a
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-03 17:03:58 +0100

    not done!

--- js/script.js
+++ js/script.js
@@ -29,27 +29,89 @@ console.log("il tempo da adesso fino alle 9:30 di domani è di " + tempo_che_ci_
 
 
 // ok, in millisecondi :D e in TEMPO UMANO NORMALE?
-    // in un minuto 60 secondi
-    // in un'ora 60 minuti (3600 secondi)
-    // in un giorno 24 ore (86400 secondi)
-let secondi_in_un_minuto = 60;
-let calcolo_orario = setInterval(nome_a_caso, 1000);
+/**
+ * 
+ * funzione per trasformare i millisecondi in secondi
+ * @param ms millisecondi da trasfomare
+ * 
+ */
+function transform_milliseconds(ms) {
+    let sec = ms / 1000;
 
-function nome_a_caso() {
-    ++secondi_in_un_minuto;
-
-    let secondi = parseInt(secondi_in_un_minuto % 60); // OK
-    // console.log(secondi);
-    let minuti = parseInt((secondi_in_un_minuto / 60) % 60); // OK
-    // console.log(minuti); 
-    let ore = parseInt((secondi_in_un_minuto / 60 / 60) % 24); // OK
-    // console.log(ore); 
-    let giorni = parseInt(secondi_in_un_minuto / 60 / 60 /24); // OK
-    // console.log(giorni);
+    return parseInt(sec);
 }
 
-// console.log("Da questo istante fino alle 9:30 di domani mancano " + )
+// trasformo millisecondi in secondi
+let my_time_in_seconds = transform_milliseconds(tempo_che_ci_serve);
+console.log("ti rimangono " + my_time_in_seconds + " secondi!");
 
-// what now?
-// BOH
 
+
+// ..and then? altre funzioni?
+/**
+ * 
+ * funzione per trasformare i secondi in minuti
+ * @param sec secondi da trasfomare
+ * 
+ */
+function transform_seconds(sec) {
+    let min = sec / 60;
+
+    return parseInt(min);
+}
+
+// trasformo secondi in minuti
+let my_time_in_minutes = transform_seconds(my_time_in_seconds);
+console.log("ti rimangono " + my_time_in_minutes + " minuti!");
+
+// funziona ma non credo sia l'idea giusta :D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*************** disastro da cancellare eventually ***************/
+/*************** not now perchè potrebbe servirmi qualcosa ***************/
+
+
+
+
+// schif:
+// // ok, in millisecondi :D e in TEMPO UMANO NORMALE?
+//     // in un minuto 60 secondi
+//     // in un'ora 60 minuti (3600 secondi)
+//     // in un giorno 24 ore (86400 secondi)
+// let secondi_in_un_minuto = 60;
+
+// // let calcolo_orario = setInterval(nome_a_caso, 1000); // THIS??????????
+
+// function nome_a_caso() {
+//     ++secondi_in_un_minuto;
+
+//     let secondi = parseInt(secondi_in_un_minuto % 60); // OK
+//     // console.log(secondi);
+//     let minuti = parseInt((secondi_in_un_minuto / 60) % 60); // OK
+//     // console.log(minuti); 
+//     let ore = parseInt((secondi_in_un_minuto / 60 / 60) % 24); // OK
+//     // console.log(ore); 
+//     let giorni = parseInt(secondi_in_un_minuto / 60 / 60 / 24); // OK
+//     // console.log(giorni);
+
+//     // return secondi, minuti, ore, giorni;
+// }
